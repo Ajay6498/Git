88e0b068a7b73d0647c1aa41ab9e88916140dcbc
Ajay6498 <ajaybawaskar19@gmail.com>
2023-02-17 00:40:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function addThreeNumber(a,s,g){
+    return a+s+g;
+}
+let c=addThreeNumber(2,64,4);
+console.log(c);
+
+function sub(a,b,){
+    return a-b;
+}
+
+let q=sub(6,55);
+console.log(q);
